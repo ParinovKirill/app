52dae211f8be6373151ce3d41a78e111f637865a
Evgeniy <zenabook@bk.ru>
2024-04-17 21:11:32 +0500

    added navigation and some animations

--- app/src/main/java/com/example/parinovkirill/Presentation/WelcomePageFragment.kt
+++ app/src/main/java/com/example/parinovkirill/Presentation/WelcomePageFragment.kt
@@ -5,20 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import androidx.viewbinding.ViewBinding
 import com.example.parinovkirill.R
 
 class WelcomePageFragment : Fragment() {
 
-    private lateinit var binding: ViewBinding
+//    private lateinit var binding: ViewBinding
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        navController = findNavController()
         return inflater.inflate(R.layout.fragment_welcome_page, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val buttonNext = view.findViewById<Button>(R.id.buttonNext)
+        buttonNext.setOnClickListener {
+            navController.navigate(R.id.action_welcomePageFragment_to_genderSelectFragment)
+        }
+    }
     companion object {
 
         @JvmStatic
